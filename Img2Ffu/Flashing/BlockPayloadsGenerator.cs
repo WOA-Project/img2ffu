a81b1e0e49cb4b91f96f3e4ad1963cbbc6f6d733
Gustave Monce <gustave.monce@outlook.com>
2024-01-14 15:41:17 +0100

    Rewrite the generation code for payloads

--- Img2Ffu/Flashing/BlockPayloadsGenerator.cs
+++ Img2Ffu/Flashing/BlockPayloadsGenerator.cs
@@ -24,7 +24,9 @@ SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Img2Ffu.Data;
 using Img2Ffu.Helpers;
 
@@ -32,7 +34,7 @@ namespace Img2Ffu.Flashing
 {
     internal class BlockPayloadsGenerator
     {
-        private static void ShowProgress(long CurrentProgress, long TotalProgress, DateTime startTime, bool DisplayRed)
+        private static void ShowProgress(long CurrentProgress, long TotalProgress, DateTime startTime)
         {
             DateTime now = DateTime.Now;
             TimeSpan timeSoFar = now - startTime;
@@ -45,7 +47,7 @@ namespace Img2Ffu.Flashing
             }
             TimeSpan remaining = TimeSpan.FromMilliseconds(milliseconds);
 
-            Logging.Log(string.Format($"{GetDismLikeProgBar(int.Parse((CurrentProgress * 100 / TotalProgress).ToString()))} {Math.Truncate(remaining.TotalHours):00}:{remaining.Minutes:00}:{remaining.Seconds:00}.{remaining.Milliseconds:000}"), returnline: false, severity: DisplayRed ? Logging.LoggingLevel.Warning : Logging.LoggingLevel.Information);
+            Logging.Log(string.Format($"{GetDismLikeProgBar(int.Parse((CurrentProgress * 100 / TotalProgress).ToString()))} {Math.Truncate(remaining.TotalHours):00}:{remaining.Minutes:00}:{remaining.Seconds:00}.{remaining.Milliseconds:000}"), returnline: false, severity: Logging.LoggingLevel.Information);
         }
 
         private static string GetDismLikeProgBar(int perc)
@@ -100,6 +102,48 @@ namespace Img2Ffu.Flashing
             }
         }
 
+        internal static Dictionary<byte[], List<ulong>> GetHashedBlocks(FlashPart[] flashParts, uint BlockSize)
+        {
+            long CurrentBlockCount = 0;
+            long TotalBlockCount = flashParts.Sum(x => x.Stream.Length / BlockSize);
+            Dictionary<byte[], List<ulong>> hashedBlocks = [];
+            DateTime startTime = DateTime.Now;
+
+            Logging.Log($"Total Block Count: {TotalBlockCount} - {TotalBlockCount * BlockSize / (1024 * 1024 * 1024)}GB");
+            Logging.Log("Hashing resources...");
+
+            foreach (FlashPart flashPart in flashParts)
+            {
+                flashPart.Stream.Seek(0, SeekOrigin.Begin);
+
+                ulong FlashPartStartBlockIndex = flashPart.StartLocation / BlockSize;
+                ulong FlashPartBlockCount = (ulong)flashPart.Stream.Length / BlockSize;
+
+                for (uint FlashPartBlockIndex = 0; FlashPartBlockIndex < FlashPartBlockCount; FlashPartBlockIndex++)
+                {
+                    byte[] BlockBuffer = new byte[BlockSize];
+                    flashPart.Stream.ReadExactly(BlockBuffer, 0, (int)BlockSize);
+                    byte[] BlockHash = SHA256.HashData(BlockBuffer);
+
+                    if (!hashedBlocks.TryGetValue(BlockHash, out List<ulong> value))
+                    {
+                        hashedBlocks.Add(BlockHash, [FlashPartStartBlockIndex + FlashPartBlockIndex]);
+                    }
+                    else
+                    {
+                        value.Add(FlashPartStartBlockIndex + FlashPartBlockIndex);
+                    }
+
+                    CurrentBlockCount++;
+                    ShowProgress(CurrentBlockCount, TotalBlockCount, startTime);
+                }
+            }
+            Logging.Log("");
+            Logging.Log($"FFU Block Count: {hashedBlocks.Count} - {hashedBlocks.Count * BlockSize / (1024 * 1024 * 1024)}GB");
+
+            return hashedBlocks;
+        }
+
         internal static Dictionary<byte[], BlockPayload> GetOptimizedPayloads(FlashPart[] flashParts, uint BlockSize, ulong MaximumNumberOfBlankBlocksAllowed)
         {
             Dictionary<byte[], BlockPayload> blockPayloads = [];
@@ -109,79 +153,44 @@ namespace Img2Ffu.Flashing
                 return blockPayloads;
             }
 
-            long TotalBlockCount = 0;
-            foreach (FlashPart flashPart in flashParts)
+            Dictionary<byte[], List<ulong>> hashedBlocks = GetHashedBlocks(flashParts, BlockSize);
+
+            Logging.Log("Building resource payloads...");
+
+            foreach (KeyValuePair<byte[], List<ulong>> hashedBlock in hashedBlocks)
             {
-                TotalBlockCount += flashPart.Stream.Length / BlockSize;
-            }
-
-            long CurrentBlockCount = 0;
-            DateTime startTime = DateTime.Now;
-
-            Logging.Log("Hashing resources...");
-
-            bool blankBlockPhase = false;
-            ulong blankBlockCount = 0;
-
-            List<(ulong DiskBlockIndex, ulong FlashPartIndex, ulong FlashPartStreamLocation)> blankBlocks = [];
-
-            for (ulong flashPartIndex = 0; flashPartIndex < (ulong)flashParts.LongLength; flashPartIndex++)
-            {
-                FlashPart flashPart = flashParts[(int)flashPartIndex];
-
-                flashPart.Stream.Seek(0, SeekOrigin.Begin);
-                ulong FlashPartBlockCount = (ulong)flashPart.Stream.Length / BlockSize;
-                ulong FlashPartStartBlockIndex = flashPart.StartLocation / BlockSize;
-
-                for (uint FlashPartBlockIndex = 0; FlashPartBlockIndex < FlashPartBlockCount; FlashPartBlockIndex++)
+                if (hashedBlock.Value.Count > 1)
                 {
-                    byte[] BlockBuffer = new byte[BlockSize];
-                    ulong BlockLocationInFlashPartStream = (ulong)flashPart.Stream.Position;
-                    flashPart.Stream.Read(BlockBuffer, 0, (int)BlockSize);
-                    byte[] BlockHash = SHA256.HashData(BlockBuffer);
+                    DiskLocation[] diskLocations = hashedBlock.Value.Select(DiskBlockIndex => new DiskLocation() { BlockIndex = (uint)DiskBlockIndex, DiskAccessMethod = 0 }).ToArray();
 
-                    ulong DiskBlockIndex = FlashPartStartBlockIndex + FlashPartBlockIndex;
-
-                    if (!ByteOperations.Compare(EMPTY_BLOCK_HASH, BlockHash))
+                    WriteDescriptor writeDescriptor = new()
                     {
-                        AddToDictionary(blockPayloads, BlockHash, DiskBlockIndex, flashPartIndex, BlockLocationInFlashPartStream);
-
-                        if (blankBlockPhase && blankBlockCount < MaximumNumberOfBlankBlocksAllowed)
+                        BlockDataEntry = new BlockDataEntry
                         {
-                            // Add the last recorded blank blocks
-                            foreach ((ulong DiskBlockIndex, ulong FlashPartIndex, ulong FlashPartStreamLocation) blankBlock in blankBlocks)
-                            {
-                                AddToDictionary(blockPayloads, EMPTY_BLOCK_HASH, blankBlock.DiskBlockIndex, blankBlock.FlashPartIndex, blankBlock.FlashPartStreamLocation);
-                            }
-                        }
+                            BlockCount = 1,
+                            LocationCount = (uint)hashedBlock.Value.Count
+                        },
+                        DiskLocations = diskLocations
+                    };
 
-                        blankBlocks.Clear();
-
-                        blankBlockPhase = false;
-                        blankBlockCount = 0;
-                    }
-                    else if (blankBlockCount < MaximumNumberOfBlankBlocksAllowed)
+                    for (ulong i = 0; i < (ulong)flashParts.LongLength; i++)
                     {
-                        blankBlocks.Add((DiskBlockIndex, flashPartIndex, BlockLocationInFlashPartStream));
+                        FlashPart flashPart = flashParts[i];
+                        ulong DiskBlockIndex = diskLocations[0].BlockIndex;
 
-                        blankBlockPhase = true;
-                        blankBlockCount++;
-                    }
-                    else if (blankBlockCount >= MaximumNumberOfBlankBlocksAllowed && blankBlocks.Count > 0)
-                    {
-                        // Add the last recorded blank blocks and clear the list
-                        foreach ((ulong DiskBlockIndex, ulong FlashPartIndex, ulong FlashPartStreamLocation) blankBlock in blankBlocks)
+                        ulong FlashPartStartBlockIndex = flashPart.StartLocation / BlockSize;
+                        ulong FlashPartBlockCount = (ulong)flashPart.Stream.Length / BlockSize;
+                        ulong FlashPartEndBlockIndex = FlashPartStartBlockIndex + FlashPartBlockCount - 1;
+
+                        if (FlashPartStartBlockIndex <= DiskBlockIndex && DiskBlockIndex <= FlashPartEndBlockIndex)
                         {
-                            AddToDictionary(blockPayloads, EMPTY_BLOCK_HASH, blankBlock.DiskBlockIndex, blankBlock.FlashPartIndex, blankBlock.FlashPartStreamLocation);
+                            blockPayloads.Add(hashedBlock.Key, new BlockPayload(writeDescriptor, i, (DiskBlockIndex - FlashPartStartBlockIndex) * BlockSize));
+                            break;
                         }
-                        blankBlocks.Clear();
                     }
 
-                    CurrentBlockCount++;
-                    ShowProgress(CurrentBlockCount, TotalBlockCount, startTime, blankBlockPhase);
                 }
             }
-            Logging.Log("");
 
             return blockPayloads;
         }
